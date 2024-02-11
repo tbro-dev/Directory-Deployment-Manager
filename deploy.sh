d2f33250cfa09fd29c5d9c1c6c6164f4b3fd2232
Tyler Broussard <tylerjbroussard@live.com>
2024-02-10 16:44:23 -0800

    Path definition function draft push 5

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #vars
+################################################
 BLUE="\033[0;36m"
 GREEN="\033[1;32m"
 YELLOW="\033[1;33m"
 RESET="\033[0m"
-TITLE="Ubuntu Deployment Manger"
+TITLE="Ubuntu Deployment Manager"
 ONE="Promote Application"
 TWO="Rollback Application"
 THREE="Display Application Version History"
@@ -13,12 +14,26 @@ FOUR="EXIT"
 user_option_input=""
 
 #procedures
+#################################################
+function validatePath(){
+        
+        local pathKey="$1"                  # First argument, example staging_path
+        local pathTitle="${pathKey//_/ }"   # Second argument, example staging path
+
+    # Check if the pathKey exists in path.txt
+        if grep -q "$pathKey" "path.txt"; then
+            staging_path=$(grep "$pathKey" "path.txt" | cut -d "=" -f 2)
+        else
+            read -p "Enter $pathTitle: " user_res
+            echo "$pathKey=$user_res" >> path.txt
+        fi
+}
+
 function pathDefinition(){
 
     # Path to the file you want to check
     file_path="./path.txt"
-    file_exists=false
-
+  
     # Check if the file exists
     if [ -f "$file_path" ]; then
         file_exists=true
@@ -29,9 +44,15 @@ function pathDefinition(){
     # Print the result
     echo "File exists: $file_exists"
 
-    if [$file_exists == true]; then
-
-
+    if [ $file_exists == true ]; then
+    
+        echo "the file path.txt exists"
+        
+        validatePath "staging_path"
+        validatePath "production_path"
+        validatePath "archive_path"
+        
+    fi
 }
 
 function displayUserOptions(){
@@ -50,45 +71,32 @@ echo -e "${BLUE}==============================================================${
 
 function captureUserOption(){
     read user_option_input
-    echo -e "You responded with: ${user_option_input}"
+    echo "${user_option_input}"
 }
 
 
-if [ $user_option_input == 1 ]; then
-    pathDefinition
-
-case $user_option_input in
-  1)
-    echo "Count is 1."
-    ;;
-  2)
-    echo "Count is 2."
-    ;;
-  3)
-    echo "Count is not 1 or 2."
-    ;;
-  4)
-    echo "Exiting application"
-    ;;
-esac
-
-# Define a function with arguments
-greet() {
-    local name="$1"   # First argument
-    local time="$2"   # Second argument
-
-    echo "Hello, $name! Good $time."
-}
-
-greet "Alice" "morning"
-
-
-testing() {
-    local arg="$1";
-
-    echo $arg
+#start application
+displayUserOptions
+user_selected_option=$(captureUserOption)
     
 
-}
-
-oneTime="true"
+case $user_selected_option in
+1)
+    pathDefinition
+    echo "User selected option 1."
+    ;;
+2)
+    pathDefinition
+    echo "User selected option 2."
+    ;;
+3)
+    pathDefinition
+    echo "User selected option 3."
+    ;;
+4)
+    echo "Exiting application"
+    ;;
+*)
+    displayUserOptions
+    ;;
+esac
